6e0a834f51e3b225831c8021f6f2ca0dd3cd812e
unknown <shahjabirtaqi@gmail.com>
2024-10-10 00:23:40 +0600

    feature: rating added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-
+import StarRating from './components/StarRating';
 function App() {
-
   return (
     <>
+      <div>
+        <StarRating maxRating={10} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
