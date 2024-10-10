f33e4825d1a3d64f4863f3084f0b65936e1a0bef
unknown <shahjabirtaqi@gmail.com>
2024-10-10 13:32:28 +0600

    feature: custom reusable rating

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import StarRating from './components/StarRating';
+
+function CustomStar() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={5} color = 'red' size = {30} className = ''  messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]} defaultRating = {0} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
       <div>
-        <StarRating maxRating={10} />
+        <StarRating />
+        <CustomStar />
       </div>
     </>
   );
