5a9fb81c008249111e58cb364d38589e88760a70
unknown <shahjabirtaqi@gmail.com>
2024-10-11 22:09:09 +0600

    Feature: E-Commerce Product with Dark Mode Toggle Added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ThemeProvider } from './contexts/Theme';
 import StarRating from './components/StarRating';
 import ToDoList from './components/ToDoList';
 import StateProjects from './components/StateProjects';
+import ThemeBtn from './components/ThemeBtn';
+import Card from './components/Card';
+function App() {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const darkTheme = () => {
+    setThemeMode('dark');
+  };
+
+  const lightTheme = () => {
+    setThemeMode('light');
+  };
+
+  useEffect(() => {
+    const htmlDOM = document.querySelector('html');
+    htmlDOM?.classList.remove('light', 'dark');
+    htmlDOM?.classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <>
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <ThemeBtn />
+        <Card />
+      </ThemeProvider>
+
+      <div id="starRating">
+        <StarRating />
+        <CustomStar />
+      </div>
+      <div id="todoList">
+        <ToDoList />
+      </div>
+      <div id="stateProject">
+        <StateProjects />
+      </div>
+    </>
+  );
+}
 
 function CustomStar() {
   const [movieRating, setMovieRating] = useState(0);
@@ -18,23 +58,6 @@ function CustomStar() {
       />
       <p>This movie was rated {movieRating} stars</p>
     </div>
-  );
-}
-
-function App() {
-  return (
-    <>
-      <div id="starRating">
-        <StarRating />
-        <CustomStar />
-      </div>
-      <div id="todoList">
-        <ToDoList />
-      </div>
-      <div id="stateProject">
-        <StateProjects />
-      </div>
-    </>
   );
 }
 
